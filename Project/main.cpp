41740e3c05edae47e0025256de855fb681bba9f9
Dmytro Hushchin <dhuchchin@gmail.com>
2021-02-20 23:36:57 +0300

    Problems with <filesystems>

---
+++ Project/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+#include <filesystem>
+
+using namespace std;
+
+int main() {
+    string dir;
+    vector <string> files_arr;
+    cout << "Enter the directory: "; cin >> dir;
+    // ! TODO: get name's list of all files (I have some problems with "filesystem" lib)
+
+
+    return 0;
+}
