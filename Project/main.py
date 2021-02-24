5c2633615b1a6a49b687c2c509ca6b3dfe7ec3ee
Dmytro Hushchin <dhuchchin@gmail.com>
2021-02-24 12:18:18 +0300

    Coverted list into matrix

--- Project/main.py
+++ Project/main.py
@@ -4,13 +4,15 @@ from os.path import isfile, join
 dir_path = input("Enter the directory path: ")
 files = [file for file in listdir(dir_path) if isfile(join(dir_path, file)) and '.csv' in file]
 print(*files)
-table = []
+lst = []
 for file in files:
     file_path = dir_path + '\\' + file
     with open(file_path) as file_handler:
         for line in file_handler:
             if line[0].isalpha():
-                table.append(line)
-    table.append('\n')
-print(*table)
+                lst.append(line)
+    lst.append('\n')
+table = [line.split(',') for line in lst]
+for el in table:
+    print(' '.join(el))
 input()
