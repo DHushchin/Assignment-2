b483b0072c459473e3b61b7fb5095ed955894bf6
Dmytro Hushchin <dhuchchin@gmail.com>
2021-02-26 02:00:52 +0300

    Added rating

--- Python/main.py
+++ Python/main.py
@@ -1,6 +1,15 @@
 from os import listdir
 from os.path import isfile, join
 
+def get_points(country, position, res):
+    if position == 0:
+        res[country].append(12)
+    elif position == 1:
+        res[country].append(10)
+    else:
+        res[country].append(10 - position)
+
+
 dir_path = input("Enter the directory path: ")
 files = [file for file in listdir(dir_path) if isfile(join(dir_path, file)) and '.csv' in file]
 print(*files)
@@ -11,8 +20,25 @@ for file in files:
         for line in file_handler:
             if line[0].isalpha():
                 lst.append(line)
-    lst.append('\n')
 table = [line.split(',') for line in lst]
-for el in table:
-    print(' '.join(el))
+result = [[table[line][0]] for line in range(len(table))]
+for j in range(1, len(table[0])):
+    tmp = []
+    for i in range(len(table)):
+        tmp.append(int(table[i][j]))
+    tmp.sort(reverse=True)
+    for k in range(len(table)):
+        for pos in range(10):
+            if int(table[k][j]) == tmp[pos]:
+                get_points(k, pos, result)
+
+for res_line in result:
+    points = 0
+    for i in range(1, len(res_line)):
+        points += int(res_line[i])
+    del res_line[1:len(res_line)]
+    res_line.append(str(points))
+
+for line in result:
+    print(line)
 input()
