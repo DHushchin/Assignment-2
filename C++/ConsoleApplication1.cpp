7be761a6da0be976e3d802b64df7b563c28376ad
Dmytro Hushchin <dhuchchin@gmail.com>
2021-02-25 19:26:19 +0300

    Optimized cpp code/fixed up .gitignore

--- C++/ConsoleApplication1.cpp
+++ C++/ConsoleApplication1.cpp
@@ -5,34 +5,28 @@
 #include <string>
 #include <fstream>
 
-std::vector<std::string> get_files(const std::string&);
-
 using namespace std;
+
+vector<string> get_files();
 
 int main() {
 	vector<pair<string, vector<int>>> data;
-	setlocale(LC_ALL, "russian");
-	string dir;
-	vector<string> files = get_files("Enter the path: "); 
+	setlocale(LC_ALL, " ");
+	vector<string> files = get_files(); 
+	system("pause>>void");
 	return 0;
 }
 
-string get_dir(const string& dir) {
-	string str;
-	cout << dir << ": ";
-	cin >> str;
-	return str;
-}
-
-vector<string> get_files(const string& dir) {
+vector<string> get_files() {
 	vector<string> files;
 	_finddata_t source;
-	string path = get_dir(dir);
-	if (path[path.size() - 1] != '\\') path += '\\';
-	intptr_t handle = _findfirst((path + "*.csv").c_str(), &source);
+	string dir;
+	cout << "Enter the directory: "; cin >> dir;
+	if (dir[dir.size() - 1] != '\\') dir += '\\';
+	intptr_t handle = _findfirst((dir + "*.csv").c_str(), &source);
 	do {
-		if (source.name != "results.csv") files.push_back(path + source.name);
-		cout << path + source.name << endl;
+		if (source.name != "results.csv") files.push_back(dir + source.name);
+		cout << dir + source.name << endl;
 	} while (_findnext(handle, &source) == 0);
 	_findclose(handle);
 	return files;
